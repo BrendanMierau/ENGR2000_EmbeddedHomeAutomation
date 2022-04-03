078303b324d46a298b3bd607fde131ba8ffeba14
BrendanMierau <36137261+BrendanMierau@users.noreply.github.com>
2022-04-03 14:09:34 -0700

    Finished the smart heating method
    This method also triggers the fire alarm

--- main.cpp
+++ main.cpp
@@ -11,15 +11,39 @@ DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 DigitalIn pir(p5);
 Timer pir_timer;
-Timer temp_timer;
 
-//For the Heat sensor
+
+//For the Smart Temperature control
 DigitalOut led3(LED3);
+DigitalOut heater_led(p7);
+DigitalOut aircon_led(p8);
 DS1820 ds1820(p6); // mbed pin name connected to module
 Serial pc(USBTX, USBRX);
+//Temp timer is for triggering the smart heating logic
+Timer temp_timer;
+//heating_timer is for the heat or aircon to stay on for 300 seconds
+Timer heating_timer;
 float temp = 0;
 int result = 0;
 bool temp_conversion = false;
+
+//For the buzzer alarm
+PwmOut buzzer(p22);
+float freq[]= {659,440,659,440,659,440,659,440,659,440,659,440};
+Timer alarm_timer;
+bool alarm_trigger = false;
+int alarm_iterator = 0;
+
+void alarm(){
+    if(alarm_trigger && (alarm_timer.read() > 0.5) && (alarm_iterator < 12)){
+        pc.printf("alarm trigger true \n\r");
+        buzzer.period(1/(2*freq[alarm_iterator]));
+        alarm_iterator++;
+        alarm_timer.reset();
+    }
+    else
+        alarm_iterator = 0;
+}
 
 void pir_sensor(){
      if (pir){
@@ -34,7 +58,14 @@ void pir_sensor(){
 }
 
 void smart_heating(){
-    if(temp_timer > 7 && !temp_conversion){
+    if(!alarm_trigger){
+        buzzer.period(0);
+    }
+    
+    if(temp > 33){
+        alarm_trigger = true;    
+    }
+    else if(temp_timer > 7 && !temp_conversion){
         ds1820.startConversion();
         temp_conversion = true;
     }
@@ -42,7 +73,7 @@ void smart_heating(){
         result = ds1820.read(temp); // read temperature
         switch (result) {
             case 0: // no errors
-                pc.printf("Temperature= %3.1f C\r\n", temp);
+                pc.printf("Temperature= %3.1f C\r\n", temp);  
                 break;
             case 1: // no sensor present
                 pc.printf("No sensor present\n\r");
@@ -52,13 +83,33 @@ void smart_heating(){
         }
         temp_conversion = false;
         temp_timer.reset();
-        led3 = !led3; // indicate update by blinking led   
+        led3 = !led3; // indicate update by blinking led
+        
+        /*
+        This checks the heater every (supposed to be 300 seconds) 20 seconds
+        and turns on the heater if its too cold or the aircon if it is too hot
+        to reach the desired temperature range
+        */
+        
+        if(heating_timer.read() > 20){
+            if(temp > 29)
+                aircon_led = 1;
+            else if(temp < 29)
+                aircon_led = 0;
+            if(temp < 27)
+                heater_led = 1;
+            else if(temp > 27)
+                heater_led = 0;   
+        }  
     }
 }
 
 int main() {
     pir_timer.start();
     temp_timer.start();
+    alarm_timer.start();
+    heating_timer.start();
+    
     pc.printf("\r\n--Starting--\r\n");
     /*
     This might be the only wait statement in the code.
@@ -68,12 +119,16 @@ int main() {
      
     wait(20); //Wait for sensor to take snap shot of still room 
     led1 = 0;
-     
+    heater_led = 0;
+    aircon_led = 0;
+    buzzer = 0.5;
+    
     pc.printf("\r\n--Starting--\r\n");
     if (ds1820.begin()){
         while(1) {
             pir_sensor();
             smart_heating();
+            alarm();
         }
     }
     else
